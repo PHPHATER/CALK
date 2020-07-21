69f68c6e4c3a855dc037dafe0a8121c83163f32c
prosvitco-genya <genya@prosvit.design>
2020-07-21 16:51:39 +0300

    mob

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" class="logo" src="./assets/logo.png" />
     <Calculator />
   </div>
 </template>
 
 <script>
-import Calculator from './components/Calculator.vue'
+import Calculator from "./components/Calculator.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Calculator
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+@media (max-width: 767.98px) {
+  .logo {
+    display: none;
+  }
+
+  .el-message-box {
+    max-width: 100%;
+  }
 }
 </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
+  <div style="margin-top: 138px;">
     <h1>OGRUZKI</h1>
     <section>
       <h3>TYT SMS CLOUD</h3>
       <div class="account" v-if="joined">
-        <img v-if="src" src="@/assets/god.jpg" alt="avatar">
-        <img v-else src="@/assets/pidr.jpg" alt="avatar">
+        <img v-if="src" src="@/assets/god.jpg" alt="avatar" />
+        <img v-else src="@/assets/pidr.jpg" alt="avatar" />
         <span>{{ user.name }}</span>
         <small>{{ user.position }}</small>
         <el-row>
-          <el-button type="success" icon="el-icon-message" :disabled="formExist" circle @click.prevent="showForm"></el-button>
+          <el-button
+            type="success"
+            icon="el-icon-message"
+            :disabled="formExist"
+            circle
+            @click.prevent="showForm"
+          ></el-button>
           <el-button icon="el-icon-switch-button" circle type="danger" @click.prevent="sebat"></el-button>
         </el-row>
       </div>
@@ -17,16 +23,28 @@
         <h2>Огрызки в сети:</h2>
         <div class="over">
           <div class="ogryzok" v-for="ogryzok in ogruzki" :key="ogryzok.id">
-          <img v-if="ogryzok.avatar == 'GOD'" src="@/assets/god.jpg" alt="avatar">
-          <img v-else src="@/assets/pidr.jpg" alt="avatar">
-          <div class="data">
-            <div class="name">Имя Огрызка: <span>{{ ogryzok.name }}</span></div>
-            <div class="position">Специальность: <small>{{ ogryzok.position }}</small></div>
+            <img v-if="ogryzok.avatar == 'GOD'" src="@/assets/god.jpg" alt="avatar" />
+            <img v-else src="@/assets/pidr.jpg" alt="avatar" />
+            <div class="data">
+              <div class="name">
+                Имя Огрызка:
+                <span>{{ ogryzok.name }}</span>
+              </div>
+              <div class="position">
+                Специальность:
+                <small>{{ ogryzok.position }}</small>
+              </div>
+            </div>
           </div>
         </div>
-        </div>
       </div>
-      <el-form :model="user" @submit.prevent.native="join('user')" v-if="!joined" :rules="rules" ref="user">
+      <el-form
+        :model="user"
+        @submit.prevent.native="join('user')"
+        v-if="!joined"
+        :rules="rules"
+        ref="user"
+      >
         <el-form-item label="Поганяло" prop="name">
           <el-input v-model="user.name"></el-input>
         </el-form-item>
@@ -42,19 +60,34 @@
       </el-form>
     </section>
     <section>
-      <div class="calculator">
-        
-      </div>
+      <div class="calculator"></div>
     </section>
     <section class="sms-grid" v-if="joined">
-      <el-tag :type="message.color" v-for="message in sms" :key="message.id" closable @close="removeSMS(message.id)">
-        {{ message.text }}
-      </el-tag>
+      <el-tag
+        :type="message.color"
+        v-for="message in sms"
+        :key="message.id"
+        closable
+        @close="removeSMS(message.id)"
+      >{{ message.text }}</el-tag>
     </section>
-    <el-form :model="smsHolder" @submit.prevent.native="sendSMS('smsHolder')" v-if="joined && formExist" :rules="rules2" ref="smsHolder" class="smsbottom">
-      <el-button icon="el-icon-close" class="form-hider" circle type="danger" @click.prevent="hideForm"></el-button>
-      <el-form-item  prop="text">
-          <el-input placeholder="Напиши Какой-то Хуеты" v-model="smsHolder.text"></el-input>
+    <el-form
+      :model="smsHolder"
+      @submit.prevent.native="sendSMS('smsHolder')"
+      v-if="joined && formExist"
+      :rules="rules2"
+      ref="smsHolder"
+      class="smsbottom"
+    >
+      <el-button
+        icon="el-icon-close"
+        class="form-hider"
+        circle
+        type="danger"
+        @click.prevent="hideForm"
+      ></el-button>
+      <el-form-item prop="text">
+        <el-input placeholder="Напиши Какой-то Хуеты" v-model="smsHolder.text"></el-input>
       </el-form-item>
       <el-form-item label="Выбери цвет смс" prop="color">
         <el-radio-group v-model="smsHolder.color">
@@ -73,7 +106,7 @@
 </template>
 
 <script>
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 export default {
   data() {
@@ -82,260 +115,342 @@ export default {
       formExist: false,
       smsHolder: {
         id: null,
-        text: '',
-        color: ''
+        text: "",
+        color: ""
       },
       user: {
         id: null,
-        name: '',
-        position: '',
-        avatar: '',
+        name: "",
+        position: "",
+        avatar: ""
       },
       ogruzki: [],
       sms: [],
       rules: {
         name: [
-          { required: true, message: 'Впиши свое поганяло, пидор!', trigger: 'blur' },
-          { min: 3, max: 40, message: 'Длина поганяла от 3х до 40 символов, пидор!', trigger: 'blur' }
+          {
+            required: true,
+            message: "Впиши свое поганяло, пидор!",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 40,
+            message: "Длина поганяла от 3х до 40 символов, пидор!",
+            trigger: "blur"
+          }
         ],
         position: [
-          { required: true, message: 'Укажи кто ты по масти, пидор!', trigger: 'change' }
+          {
+            required: true,
+            message: "Укажи кто ты по масти, пидор!",
+            trigger: "change"
+          }
         ]
       },
       rules2: {
         text: [
-          { required: true, message: 'Впиши свое смс, пидор!', trigger: 'blur' },
-          { min: 3, max: 20, message: 'Длина смс от 3х до 20 символов, пидор!', trigger: 'blur' }
+          {
+            required: true,
+            message: "Впиши свое смс, пидор!",
+            trigger: "blur"
+          },
+          {
+            min: 3,
+            max: 20,
+            message: "Длина смс от 3х до 20 символов, пидор!",
+            trigger: "blur"
+          }
         ],
         color: [
-          { required: true, message: 'Укажи цвет смс, пидор!', trigger: 'change' }
+          {
+            required: true,
+            message: "Укажи цвет смс, пидор!",
+            trigger: "change"
+          }
         ]
       },
-      socket: io('https://kalkulate.herokuapp.com/') // io('localhost:5000')
-    }
+      socket: io("https://kalkulate.herokuapp.com/") // io('localhost:5000')
+    };
   },
   created() {
-    window.addEventListener('beforeunload', this.sebat)
+    window.addEventListener("beforeunload", this.sebat);
   },
   mounted() {
-    this.socket.on('state', data => {
-      this.ogruzki =  data
-    })
-    this.socket.on('state2', data => {
-      this.sms = data
-    })
+    this.socket.on("state", data => {
+      this.ogruzki = data;
+    });
+    this.socket.on("state2", data => {
+      this.sms = data;
+    });
   },
   watch: {
     position(val) {
-      this.user.avatar = 'PIDOR'
-      if (val == 'JS') {
-        this.user.avatar = 'GOD'
+      this.user.avatar = "PIDOR";
+      if (val == "JS") {
+        this.user.avatar = "GOD";
       }
-      if (val != ' ') {
-        this.user.id = Date.now()
+      if (val != " ") {
+        this.user.id = Date.now();
       }
     }
   },
   computed: {
     src() {
-      if (this.user.position == 'JS') {
-        return true
+      if (this.user.position == "JS") {
+        return true;
       } else {
-        return false
+        return false;
       }
     },
     position() {
-      return this.user.position
+      return this.user.position;
     }
   },
   methods: {
     showForm() {
-      this.formExist = true
+      this.formExist = true;
     },
     hideForm() {
-      this.formExist = false
+      this.formExist = false;
     },
     join(formName) {
       this.$refs[formName].validate(valid => {
         if (valid) {
-          this.socket.emit('join', {
+          this.socket.emit("join", {
             id: this.user.id,
             name: this.user.name,
             position: this.user.position,
             avatar: this.user.avatar
-          })
-          this.joined = true
+          });
+          this.joined = true;
         } else {
-          this.joined = false
-          this.$alert('Слышь, пидрила. Соберись нахуй', 'Обнаружен Пидор', {
-          confirmButtonText: 'OK',
-          callback: () => {
-            this.$message({
-              type: 'error',
-              message: 'Постарайся не быть пидором и заполни поля'
-            });
-          }
-        });
+          this.joined = false;
+          this.$alert("Слышь, пидрила. Соберись нахуй", "Обнаружен Пидор", {
+            confirmButtonText: "OK",
+            callback: () => {
+              this.$message({
+                type: "error",
+                message: "Постарайся не быть пидором и заполни поля"
+              });
+            }
+          });
         }
-      })
+      });
     },
     sendSMS(formName) {
-      this.smsHolder.id = Date.now()
+      this.smsHolder.id = Date.now();
       this.$refs[formName].validate(valid => {
         if (valid) {
-          this.socket.emit('sms', {
+          this.socket.emit("sms", {
             id: this.smsHolder.id,
             text: this.smsHolder.text,
             color: this.smsHolder.color
-          })
-          this.smsHolder.id = null
-          this.smsHolder.text = ''
-          this.smsHolder.color = ''
+          });
+          this.smsHolder.id = null;
+          this.smsHolder.text = "";
+          this.smsHolder.color = "";
         } else {
-          this.$alert('Слышь, пидрила. Соберись нахуй', 'Обнаружен Пидор', {
-          confirmButtonText: 'OK',
-          callback: () => {
-            this.$message({
-              type: 'error',
-              message: 'Постарайся не быть пидором'
-            });
-          }
-        });
+          this.$alert("Слышь, пидрила. Соберись нахуй", "Обнаружен Пидор", {
+            confirmButtonText: "OK",
+            callback: () => {
+              this.$message({
+                type: "error",
+                message: "Постарайся не быть пидором"
+              });
+            }
+          });
         }
-      })
+      });
     },
     sebat() {
-      this.socket.emit('sebat', {
+      this.socket.emit("sebat", {
         name: this.user.name,
         id: this.user.id
-      })
-      this.joined = false
-      this.user.name = ''
-      this.user.id = ''
-      this.user.position = ''
+      });
+      this.joined = false;
+      this.user.name = "";
+      this.user.id = "";
+      this.user.position = "";
     },
     removeSMS(id) {
-      this.socket.emit('deleteSMS', {
+      this.socket.emit("deleteSMS", {
         id: id
-      })
+      });
     }
   },
   destroyed() {
-    this.sebat()
+    this.sebat();
   }
-}
+};
 </script>
 
 <style>
 .el-form {
-    max-width: 600px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 50px;
-    background: #000;
-    border-radius: 10px;
+  max-width: 600px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 50px;
+  background: #000;
+  border-radius: 10px;
 }
 .form-hider {
-    position: absolute;
-    top: -25px;
-    left: 25px;
+  position: absolute;
+  top: -25px;
+  left: 25px;
 }
 .el-form-item__label {
-    color: #fff;
+  color: #fff;
 }
 .account img {
-    width: 100px;
-    border-radius: 50%;
-    margin: auto;
+  width: 100px;
+  border-radius: 50%;
+  margin: auto;
 }
 
 .account {
-    position: fixed;
-    top: 20px;
-    left: 20px;
-    text-align: center;
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  text-align: center;
 }
 
 .account span {
-    display: block;
-    margin: 10px 0 5px;
-    text-transform: uppercase;
-    font-weight: 900;
-    line-height: 1;
+  display: block;
+  margin: 10px 0 5px;
+  text-transform: uppercase;
+  font-weight: 900;
+  line-height: 1;
 }
 
 .account small {
-    color: #fff;
-    font-weight: 700;
-    font-size: 12px;
-    position: absolute;
-    top: 0;
-    right: 0;
-    background: red;
-    padding: 5px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 12px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: red;
+  padding: 5px;
 }
 .ogruzochki {
-    position: fixed;
-    top: 20px;
-    right: 20px;
-    width: 30%;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  width: 30%;
 }
 
 .ogryzok {
-    display: flex;
-    flex-wrap: wrap;
+  display: flex;
+  flex-wrap: wrap;
 }
 
 .ogryzok img {
-    width: 55px;
-    border-radius: 50%;
-    flex: 0 0 55px;
+  width: 55px;
+  border-radius: 50%;
+  flex: 0 0 55px;
 }
 .ogruzochki {
-    text-align: left;
+  text-align: left;
 }
 .ogruzochki {
-    text-align: left;
+  text-align: left;
 }
 
 .data {
-    flex: 0 0 calc(100% - 55px);
-    padding-left: 20px;
-    box-sizing: border-box;
+  flex: 0 0 calc(100% - 55px);
+  padding-left: 20px;
+  box-sizing: border-box;
 }
 
 .ogryzok {
-    align-items: center;
-    padding: 10px 20px;
+  align-items: center;
+  padding: 10px 20px;
 }
 
 .ogryzok:nth-child(even) {
-    background: #eee;
+  background: #eee;
 }
 
 .ogruzochki {
-    padding: 15px;
-    background: #ccc;
+  padding: 15px;
+  background: #ccc;
 }
 
 body {
-    margin: 0!important;
+  margin: 0 !important;
 }
 
 .ogruzochki .over {
-    max-height: calc(100% - 80px);
-    overflow-y: auto;
-    margin-left: -15px;
-    margin-right: -15px;
+  max-height: calc(100% - 80px);
+  overflow-y: auto;
+  margin-left: -15px;
+  margin-right: -15px;
 }
 .el-form.smsbottom {
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    width: 100%;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  max-width: 100%;
+  box-sizing: border-box;
+  padding-bottom: 0;
+  border-radius: 0;
+}
+
+@media (max-width: 767.98px) {
+  .ogruzochki {
+    top: 0;
+    right: 0;
+    width: calc(100% - 180px);
+  }
+
+  .ogruzochki h2 {
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  .ogruzochki .over {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .ogruzochki .over .ogryzok {
+    flex: 0 0 50px;
+    padding: 5px;
+    max-width: 50px;
+    position: relative;
+    background: transparent;
+  }
+
+  .ogryzok img {
     max-width: 100%;
-    box-sizing: border-box;
-    padding-bottom: 0;
-    border-radius: 0;
+    position: relative;
+  }
+
+  .data {
+    padding: 0;
+  }
+
+  .name {
+    font-size: 0;
+  }
+
+  .name span {
+    font-size: 12px;
+    background: red;
+    color: #fff;
+    position: absolute;
+    top: 0;
+    padding: 1px 5px;
+    font-weight: 700;
+    left: 0;
+  }
+
+  .position {
+    display: none;
+  }
 }
 </style>
